4cb783ceff608ffe6d7dad6d999c5d0c291546f7
Nguyễn Hoàng Phúc <trus2lun@gmail.com>
2019-10-24 13:42:36 +0700

    session1

--- session1/hello.php
+++ session1/hello.php
@@ -1,3 +1,67 @@
 <?php
 	echo "<h1>Hello world!</h1>";
 ?>
+<h2>h2 tag</h2>
+<?php
+	$userName = 'php03' ;
+	echo $userName;
+	echo "<br>";
+?>
+<?php
+	$userName = 'Nguyễn Hoàng Phúc';
+	echo $userName;
+	echo "<br>";
+	$country = 'Đà Nẵng';
+	echo $country;
+	echo "<br>";
+	$phone = '0903';
+	echo $phone;
+	echo "<br>";
+?>
+<?php
+	$month = 12;
+	if (($month<1)||($month>12)) {
+		echo "Không phải tháng trong năm. ";
+	}
+	switch ($month) {
+		case 1:
+			echo " tháng 1 có 31 ngày ";
+			break;
+		case 2:
+			echo " tháng 2 có 28 hoặc 29 ngày";
+			break;
+		case 3:
+			echo " tháng 3 có 31 ngày ";
+			break;
+		case 4:
+			echo " tháng 4 có 30 ngày ";
+			break;
+		case 5:
+			echo " tháng 5 có 31 ngày ";
+			break;
+		case 6:
+			echo " tháng 6 có 30 ngày ";
+			break;
+		case 7:
+			echo " tháng 7 có 31 ngày ";
+			break;
+		case 8:
+			echo " tháng 8 có 31 ngày ";
+			break;
+		case 9:
+			echo " tháng 9 có 30 ngày ";
+			break;
+		case 10:
+			echo " tháng 10 có 31 ngày ";
+			break;
+		case 11:
+			echo " tháng 11 có 30 ngày ";
+			break;
+		case 12:
+			echo " tháng 12 có 31 ngày ";
+			break;
+		default:
+			echo " Nhập lại bạn ei";
+			break;
+	}
+?>
