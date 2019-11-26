b9a3127853da41a06ebf5d4e208d210363e43432
Nguyễn Hoàng Phúc <trus2lun@gmail.com>
2019-11-26 18:31:46 +0700

    update

--- session6/update.php
+++ session6/update.php
@@ -1,8 +1,91 @@
-<?php include 'database.php';?>
-<?php
-	$id = $_GET['ID'];
-	$sqlUpdate = "UPDATE input SET label ='Hồ Gươm' WHERE ID = 5";
-	if (mysqli_query($connect, $sqlUpdate) === TRUE) {
-		// chuyển trang
-		header('Location: list_users.php');
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>UPDATE</title>
+	<meta charset="utf-8">
+	<style type="text/css">
+		span {
+			color: red;
+		}
+		h1 {
+			text-align: center;
+		}
+		.input-form {
+			width: 60%;
+			margin: 0 auto;
+			border: 1px solid black;
+			height: 200px;
+		}
+		.form-control {
+			margin: 20px;
+			height: 30px;
+		}
+		.label {
+			width: 20%;
+			float: left;
+		}
+		.input {
+			width: 70%;
+			float: left;
+		}
+		.error {
+			color: red;
+		}
+		img {
+			width: 150px;
+		}
+	</style>
+	<meta name="keywords" content="PHP, mysql, SDC">
+	<meta name="decription" content="This is register form">
+</head>
+<body>
+	<?php include 'database.php';?>
+	<?php
+	//get old user to edit
+	$idUpdate = $_GET['ID'];
+
+	$sqlUpdate = "SELECT * FROM input WHERE ID = $idUpdate";
+	$dataUpdate = mysqli_query($connect, $sqlUpdate);
+	$update = $dataUpdate->fetch_assoc();
+	//end get old
+	if (isset($_POST['update'])) {
+		$label = $_POST['label'];
+		$DescribeD = $_POST['describe'];
+		$image = $update['image'];
+		if ($_FILES['image']['error'] == 0) {
+			$image = $_FILES['image']['name'];
+			move_uploaded_file($_FILES['image']['tmp_name'],'uploads/'.$image);
+		}
+
+		$sqlUpdateData = "UPDATE input SET label = '$label', DescribeD = '$describe', image = '$image' WHERE ID = $idUpdate";
+
+		if ($connect->query($sqlUpdateData) === TRUE) {
+			header('Location: list_users.php');
+		}
+	}
+	?>
+	<h1>Update</h1>
+	<div class="input-form">
+		<form action="#" name="inputForm" id="inputForm" method="POST" enctype="multipart/form-data" style="margin-left: 30%;">
+			<div class="label">Label</div>
+			<div class="input">
+				<input type="text" name="label" id="label" value="<?php echo $update['label'];?>">
+			</div>
+			<div class="label">Describe</div>
+			<div class="input">
+				<input type="text" name="describe" id="describe" value="<?php echo $update['DescribeD'];?>"> 
+			</div>
+			<div class="label">Image</div>
+			<div class="input">
+				<input type="file" name="image" id="image">
+				<img src="uploads/<?php echo $update['image'];?>">
+			</div>
+			<div class="form-control">
+				<div class="input">
+					<input type="submit" name="update" value="Update">
+				</div>
+			</div>
+		</form>
+	</div>
+</body>
+</html>
